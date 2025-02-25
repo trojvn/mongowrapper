ca465053a434b2f377434db5e151604fd8d532f7
trojvn <mrx.trojan@gmail.com>
2025-02-25 22:28:03 +0300

    fix: more def timeout

--- mongowrapper/models.py
+++ mongowrapper/models.py
@@ -8,7 +8,7 @@ class MongoOptions:
     db_name: str = ""
     host: str = "db1.yuharan.ru"
     port: int = 27017
-    timeout: int = 60000
+    timeout: int = 300000
 
     def __post_init__(self):
         if not self.db_name:
